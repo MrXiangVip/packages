56d7e13cbb5a90fdda5ffd7179fa3d9d3c803b9d
xshx <mrxiangvip@163.com>
2023-02-27 09:31:49 +0800

    [设置][删除设置 ]

--- Settings/app/src/main/java/com/example/settings/SettingsPreferenceFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.settings;
-
-import com.example.settings.core.InstrumentedPreferenceFragment;
-
-public  abstract class SettingsPreferenceFragment extends InstrumentedPreferenceFragment {
-}
