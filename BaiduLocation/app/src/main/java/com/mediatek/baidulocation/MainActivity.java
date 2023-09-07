dc325e576ed03cbd07eefcc39ece17b07fd2f9be
xshx <mrxiangvip@163.com>
2023-09-07 17:46:55 +0800

    [百度定位][增加文本展示]

--- BaiduLocation/app/src/main/java/com/mediatek/baidulocation/MainActivity.java
+++ BaiduLocation/app/src/main/java/com/mediatek/baidulocation/MainActivity.java
@@ -3,7 +3,9 @@ package com.mediatek.baidulocation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
@@ -13,14 +15,16 @@ import com.baidu.location.LocationClientOption;
 public class MainActivity extends AppCompatActivity {
 
     private String TAG="MainActivity.";
-
+    private TextView locateText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        locateText = findViewById( R.id.locate_text );
         try {
             initLocationOption();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initLocationOption() throws Exception {
         LocationClient.setAgreePrivacy(true);
+
 //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
         LocationClient locationClient = new LocationClient(getApplicationContext());
 //声明LocationClient类实例并配置定位参数
@@ -84,22 +89,51 @@ public class MainActivity extends AppCompatActivity {
             //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
             //以下只列举部分获取经纬度相关（常用）的结果信息
             //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            StringBuffer sb = new StringBuffer(1024);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nsysTime : ");
+            sb.append(SystemClock.elapsedRealtime());
+            sb.append("\nlocType : ");// 定位类型
+            sb.append(location.getLocType());
+            sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+            sb.append(location.getLocTypeDescription());
+            sb.append("\nlatitude : ");// 纬度
+            sb.append(location.getLatitude());
+            sb.append("\nlongtitude : ");// 经度
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");// 半径
+            sb.append(location.getRadius());
+            sb.append("\nCountryCode : ");// 国家码
+            sb.append(location.getCountryCode());
+            sb.append("\nProvince : ");// 获取省份
+            sb.append(location.getProvince());
+            sb.append("\nCountry : ");// 国家名称
+            sb.append(location.getCountry());
+            sb.append("\ncitycode : ");// 城市编码
+            sb.append(location.getCityCode());
+            sb.append("\ncity : ");// 城市
+            sb.append(location.getCity());
+            sb.append("\nDistrict : ");// 区
+            sb.append(location.getDistrict());
+            sb.append("\nTown : ");// 获取镇信息
+            sb.append(location.getTown());
+            sb.append("\nStreet : ");// 街道
+            sb.append(location.getStreet());
+            sb.append("\naddr : ");// 地址信息
+            sb.append(location.getAddrStr());
+            sb.append("\nStreetNumber : ");// 获取街道号码
+            sb.append(location.getStreetNumber());
+            sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+            sb.append(location.getUserIndoorState());
+            sb.append("\nDirection(not all devices have value): ");
+            sb.append(location.getDirection());// 方向
+            sb.append("\nlocationdescribe: ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            sb.append("\nPoi: ");// POI信息
+            Log.d(TAG, " "+sb.toString());
 
-            //获取纬度信息
-            double latitude = location.getLatitude();
-            //获取经度信息
-            double longitude = location.getLongitude();
-            //获取定位精度，默认值为0.0f
-            float radius = location.getRadius();
-            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
-            String coorType = location.getCoorType();
-            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-            int errorCode = location.getLocType();
-            Log.d(TAG, " errorCode "+errorCode);
-            Log.d(TAG, " latitude "+latitude);
-            Log.d(TAG, " longitude "+longitude);
-            Log.d(TAG, " radius "+radius);
-            Log.d(TAG, " coorType "+coorType);
+            locateText.setText( sb);
         }
     }
 }
